ac82271c1b0ce8815034cbda1afdf40fc240cd35
Osama Bsharat <bsharat1999@gmail.com>
2024-03-16 21:29:13 +0200

    e-commerce-app-constants-commit

---
+++ lib/Screens/Splash/splash.dart
@@ -0,0 +1,13 @@
+import 'package:ecommerceapp/constants.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
