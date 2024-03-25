de0b51c9dd76498f04f8f2d8c9f1b1d5a5c04e2e
Osama Bsharat <bsharat1999@gmail.com>
2024-03-25 22:11:05 +0200

    e-commerce-link-with-firebase-udpation-commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:ecommerceapp/Screens/Onboarding/onboarding.dart';
 import 'package:ecommerceapp/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
