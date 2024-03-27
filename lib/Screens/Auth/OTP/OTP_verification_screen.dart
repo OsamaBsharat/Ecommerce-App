a56d633053d72662e2d6fff5bf69c6d4e7d72172
Osama Bsharat <bsharat1999@gmail.com>
2024-03-27 22:28:27 +0200

    e-commerce-app-Auth-otp-verification-done-1-commit

--- lib/Screens/Auth/OTP/OTP_verification_screen.dart
+++ lib/Screens/Auth/OTP/OTP_verification_screen.dart
@@ -90,7 +90,7 @@ class _OTPVerificationScreenState extends State<OTPVerificationScreen> {
             ),
             GestureDetector(
               child:
-                  Text("Resent new ode", style: TextStyle(color: Colors.white)),
+                  Text("Resent new Code", style: TextStyle(color: Colors.white)),
               onTap: () {},
             ),
             SizedBox(
@@ -98,7 +98,7 @@ class _OTPVerificationScreenState extends State<OTPVerificationScreen> {
             ),
             RoundedButton(
               size: size,
-              text: "Next",
+              text: "Verfiy",
               press: () {
                 Navigator.push(
                   context,
